4d78eaecf656b803db3082fc5f581e132fab1b71
Володимир <volodymyr.logika@gmail.com>
2024-05-11 12:31:15 +0300

    урок 1

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
 
 
-class MainApp(MDApp):
+class MainScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class WeatherApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, Kivy", halign="center")
+        Builder.load_file('style.kv')
+        self.theme_cls.theme_style = "Dark"
+        self.screen = MainScreen("main_screen")
+        return self.screen
 
-
-MainApp().run()
+WeatherApp().run()
