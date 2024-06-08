80eef109576bbbdf15844d0dee2d76baace1d2b3
Володимир <volodymyr.logika@gmail.com>
2024-06-08 03:10:28 +0300

    v1

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from kivymd.uix.pickers.datepicker.datepicker import date
 from kivymd.uix.backdrop.backdrop import MDCard
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.screen import MDScreen
 import requests
 
-from config import API_KEY, API_URL
+from config import API_KEY, API_URL, FORECAST_URL
 
 class WeatherCard(MDCard):
-    def __init__(self , description, icon, temp, rain, wind, *args, **kwargs):
+    def __init__(self ,date_text, description, icon, temp, rain, wind, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.ids.desc_text.text = description
+        self.ids.date_text.text = date_text
+        self.ids.desc_text.text = description.capitalize()
         self.ids.temp_text.text = f"{temp}°C"
         self.ids.rain_text.text = f"Ймовірність опадів: {rain*100}%"
         self.ids.wind_text.text = f"Швидкість вітру: {wind} м/c"
@@ -23,27 +27,47 @@ class MainScreen(MDScreen):
 
     def weather_search(self):
         self.ids.weather_carousel.clear_widgets()
-        city = self.ids.city_field.text.strip().lower() #отримує з текостового поля назву міста
-        api_params = { 
-            "q": city, 
-            "appid": API_KEY
-        }
-        
-        data = requests.get(API_URL, api_params) # робимо запит по АПІ до сервісу погоди
-        response = data.json() # отримуємо дані про погодуу форматі JSON
-        print(response) 
-        description = response["weather"][0]["description"] #опис погоди
-        icon =  response["weather"][0]["icon"] # інонка
-        temp = response["main"]["temp"] #температура
-        if 'rain' in response:
-            rain = response["rain"]["1h"]
+        city = self.ids.city_field.text.strip().lower()
+        current_weather = self.get_weather_data(API_URL, city)
+        forecast = self.get_weather_data(FORECAST_URL, city)
+
+        if current_weather:
+            self.add_weather_card("Зараз", current_weather)
+
+        if forecast:
+            for period in forecast['list'][1::2]:
+                date = period['dt_txt'][5:-3]
+                self.add_weather_card(date, period)
+
+    def get_weather_data(self, url, city):
+        api_params = {"q": city, "appid": API_KEY}
+        response = requests.get(url, api_params)
+        if response.status_code == 200:
+            return response.json()
         else:
-            rain = 0
-        wind = response["wind"]["speed"]        
-        new_card = WeatherCard(description, icon, temp, rain, wind)
-        self.ids.weather_carousel.add_widget(new_card)   
+            return None
 
+    def add_weather_card(self, date, data):
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+        rain = data.get('rain', {}).get('1h', 0) if date == "Зараз" else data.get('rain', {}).get('3h', 0)
+        wind = data['wind']['speed']
+        new_card = WeatherCard(date, description, icon, temp, rain, wind)
+        self.ids.weather_carousel.add_widget(new_card)
 
+    def show_about_dialog(self):
+        dialog = MDDialog(
+            title="Про розробника",
+            text="Цей додаток був розроблений в школі Logika. Для контакту: ваш_email@example.com",
+            buttons=[
+                MDRaisedButton(
+                    text="Закрити",
+                    on_release=lambda x: dialog.dismiss()
+                )
+            ]
+        )
+        dialog.open()
 
 
 class WeatherApp(MDApp):
